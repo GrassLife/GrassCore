1b2504b906d48a7a82f0b8a2fc44f2dc14a633ee
gurapomu <gurapomu@gmail.com>
2016-08-24 02:40:17 +0900

    開発環境の構築

---
+++ src/java/life/grass/grasscore/Grasscore.java
@@ -0,0 +1,21 @@
+package life.grass.grasscore;
+
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public class Grasscore extends JavaPlugin {
+    public static Logger log;
+    public static PluginManager pluginManager;
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
