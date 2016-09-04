39118c54a3350c7be80481bf82559dffec17ced5
BuildTools <unconfigured@null.spigotmc.org>
2016-09-05 04:41:52 +0900

    [add]プレイヤー釣りイベント

--- src/java/life/grass/grasscore/player/event/PlayerFishingEventGC.java
+++ src/java/life/grass/grasscore/player/event/PlayerFishingEventGC.java
@@ -1,8 +1,13 @@
 package life.grass.grasscore.player.event;
 
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 
@@ -11,10 +16,14 @@ import org.bukkit.event.player.PlayerLoginEvent;
  */
 public class PlayerFishingEventGC implements Listener {
     @EventHandler
-    public void onPlayerFishing(PlayerFishEvent event){
-        if (event.getState().equals(PlayerFishEvent.State.CAUGHT_FISH)) {
+    public void onPlayerFishing(PlayerFishEvent event) {
+        if (event.getState() == PlayerFishEvent.State.CAUGHT_FISH) {
+            event.getCaught().remove();
             System.out.println("FISH!!");
+            /*ハズレ*/
+
         }
 
     }
+
 }
