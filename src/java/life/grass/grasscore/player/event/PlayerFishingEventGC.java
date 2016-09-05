4b0f67580c49786dc8b175287135d79a8f5b4767
BuildTools <unconfigured@null.spigotmc.org>
2016-09-06 03:46:04 +0900

    [add]確率でスカor生肉を釣れる

--- src/java/life/grass/grasscore/player/event/PlayerFishingEventGC.java
+++ src/java/life/grass/grasscore/player/event/PlayerFishingEventGC.java
@@ -1,15 +1,18 @@
 package life.grass.grasscore.player.event;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Created by cyclicester on 2016/09/03.
@@ -18,9 +21,15 @@ public class PlayerFishingEventGC implements Listener {
     @EventHandler
     public void onPlayerFishing(PlayerFishEvent event) {
         if (event.getState() == PlayerFishEvent.State.CAUGHT_FISH) {
-            event.getCaught().remove();
-            System.out.println("FISH!!");
-            /*ハズレ*/
+            int randomnumber = new java.util.Random().nextInt(10);
+            Item gottenfish = (Item) event.getCaught();
+            if(randomnumber < 5) {
+                gottenfish.remove();
+                /*はずれ*/
+            } else {
+                gottenfish.setItemStack(new ItemStack(Material.RAW_BEEF));
+                /*あたり*/
+            }
 
         }
 
