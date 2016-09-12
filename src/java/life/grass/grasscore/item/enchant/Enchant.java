4b6c2f22c629e5da9077179706a0c100fcc15a87
ecilavip <ecilavip@outlook.jp>
2016-09-12 21:50:52 +0900

    [add] エンチャントクラスの追加

--- src/java/life/grass/grasscore/item/enchant/Enchant.java
+++ src/java/life/grass/grasscore/item/enchant/Enchant.java
@@ -1,8 +1,47 @@
 package life.grass.grasscore.item.enchant;
 
+import life.grass.grasscore.item.tags.ItemTag;
+
+import java.util.ArrayList;
+
 /**
  * Created by Ecila on 2016/09/03.
  */
 public class Enchant {
+    private int id;
+    private String label;
+    private EnchantPosition position;
+    private ArrayList<ItemTag> efficacies = new ArrayList<>();
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public EnchantPosition getPosition() {
+        return position;
+    }
+
+    public void setPosition(EnchantPosition position) {
+        this.position = position;
+    }
+
+    public ArrayList<ItemTag> getEfficacies() {
+        return efficacies;
+    }
+
+    public void setEfficacies(ArrayList<ItemTag> efficacies) {
+        this.efficacies = efficacies;
+    }
 }
---
+++ src/java/life/grass/grasscore/item/enchant/EnchantPosition.java
@@ -0,0 +1,10 @@
+package life.grass.grasscore.item.enchant;
+
+/**
+ * Created by Ecila on 2016/09/12.
+ */
+public enum EnchantPosition {
+    PREFIX,
+    SPECIAL,
+    SUFFIX;
+}
