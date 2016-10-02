329cf220b6ebbe3f4079a0ee87ee3affc499521f
Cyclic_Ester <unconfigured@null.spigotmc.org>
2016-10-02 10:11:45 +0900

    [improve]より本番に近い処理に近づけた

--- src/java/life/grass/grasscore/fishing/FishingManager.java
+++ src/java/life/grass/grasscore/fishing/FishingManager.java
@@ -1,6 +1,13 @@
 package life.grass.grasscore.fishing;
 
+import org.bukkit.Material;
+import org.bukkit.WeatherType;
+import org.bukkit.block.Biome;
+import org.bukkit.entity.Weather;
+import org.bukkit.inventory.ItemStack;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -8,44 +15,69 @@ import java.util.List;
  */
 public class FishingManager {
 
-    private static List<String> fitemList = makeItemList();
-    private static List<Double> ratioList = makeRatioList();
-    private static List<Double> rsumList = makeSumList(ratioList);
+    private static List<FishableItem> fishList = makeItemList();
+    private static HashMap<FishableItem, ItemStack> fitemMap = makeFitemMap();
+    private static List<Double> failList = makeFailList();
 
-    //実装時はコンフィグからアイテムのリストを作る予定。
-    public  static List<String> makeItemList() {
-        List<String> list = new ArrayList<>();
-        list.add("鶏肉");
-        list.add("牛肉");
-        list.add("牛肉のような何か");
+    /*
+    実装時はコンフィグからアイテムのリストを作る予定。
+     */
+    public static List<FishableItem> makeItemList() {
+        List<FishableItem> list = new ArrayList<>();
+        for (int i = 0; i <= 2; i++) {
+            list.add(new FishableItem(1));
+        }
         return list;
     }
 
-    //実装時には釣りをしているときのバイオームと天候などを渡してgetRealratioにより比取得して対応するindexに入れていきます。
-    public static ArrayList<Double> makeRatioList(){
+    public static HashMap<FishableItem, ItemStack> makeFitemMap(){
+        HashMap<FishableItem, ItemStack> map = new HashMap<>();
+        map.put(fishList.get(0), new ItemStack(Material.RAW_BEEF));
+        map.put(fishList.get(1), new ItemStack(Material.RAW_CHICKEN));
+        map.put(fishList.get(2), new ItemStack(Material.RAW_FISH));
+        return map;
+    }
+
+    public static List<Double> makeFailList(){
+        List<Double> list = new ArrayList<>();
+        list.add(5.0);
+        list.add(5.0);
+        return list;
+    }
+
+    /*
+    釣りをしているときのバイオームと天候などを渡してgetRealratioにより比取得して対応するindexに入れていきます。
+     */
+    public static ArrayList<Double> makeRatioList(Biome b, WeatherType w) {
         ArrayList<Double> list = new ArrayList<>();
-        list.add(7.0);
-        list.add(2.0);
-        list.add(1.0);
+        for (int i = 0; i < fishList.size(); i++) {
+            list.add(fishList.get(i).getRealratio(b, w));
+        }
         return list;
     }
-    //リストを渡すと各々のインデックスまでの総和を要素に持つ比のリストを生成します
-    public static List<Double> makeSumList(List<Double> list){
+
+    /*
+    リストを渡すと各々のインデックスまでの総和を要素に持つ比のリストを生成します
+     */
+    public static List<Double> makeSumList(List<Double> list) {
         List<Double> sumList = new ArrayList<>();
-        for(int i= 0; i < list.size() ; i++){
-            Double left = i == 0? 0.0: sumList.get(i - 1);
+        for (int i = 0; i < list.size(); i++) {
+            Double left = i == 0 ? 0.0 : sumList.get(i - 1);
             sumList.add(left + list.get(i));
         }
-        return sumList ;
+        return sumList;
     }
-    //比のリストを渡すとそれに従ってランダムなindexを返してくれます。
-    public static int probMaker( List<Double> list){
+
+    /*
+    比のリストを渡すとそれに従ってランダムなindexを返してくれます。
+     */
+    public static int probMaker(List<Double> list) {
         double random = Math.random() * list.get(list.size() - 1);
         System.out.println(random);
         int indexNumber = 0;
-        for(int i = 0 ; i < list.size() ; i++){
-            double left = i == 0? 0: list.get(i - 1);
-            if(random >= left && random < list.get(i) ){
+        for (int i = 0; i < list.size(); i++) {
+            double left = i == 0 ? 0 : list.get(i - 1);
+            if (random >= left && random < list.get(i)) {
                 indexNumber = i;
             }
         }
@@ -53,15 +85,13 @@ public class FishingManager {
         return indexNumber;
     }
 
-    public static List<Double> getRatioList() {
-        return ratioList;
+
+
+    public static List<FishableItem> getFishList() {
+        return fishList;
     }
 
-    public static List<Double> getRsumList() {
-        return rsumList;
-    }
-
-    public static List<String> getFitemList() {
-        return fitemList;
+    public static HashMap<FishableItem, ItemStack> getFitemMap() {
+        return fitemMap;
     }
 }
--- src/java/life/grass/grasscore/fishing/event/PlayerFishingEventGC.java
+++ src/java/life/grass/grasscore/fishing/event/PlayerFishingEventGC.java
@@ -1,18 +1,15 @@
 package life.grass.grasscore.fishing.event;
 
+import life.grass.grasscore.fishing.FishableItem;
 import life.grass.grasscore.fishing.FishingManager;
-import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
+import org.bukkit.WeatherType;
+import org.bukkit.block.Biome;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.Weather;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.event.player.PlayerFishEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
@@ -24,25 +21,25 @@ import java.util.List;
 public class PlayerFishingEventGC implements Listener {
     @EventHandler
     public void onPlayerFishing(PlayerFishEvent event) {
+        Biome playerBiome = event.getPlayer().getLocation().getWorld()
+                .getBiome(event.getPlayer().getLocation().getBlockX(), event.getPlayer().getLocation().getBlockZ());
+        WeatherType playerWeather = event.getPlayer().getPlayerWeather();
         event.setExpToDrop(0);
 
         if (event.getState() == PlayerFishEvent.State.CAUGHT_FISH) {
             Item gottenFish = (Item) event.getCaught();
-            List<Double> nope = FishingManager.makeSumList(makeNope());//ここはratioListから取ってくる。
+            List<Double> nope = FishingManager.makeSumList(FishingManager.makeFailList());
             if(FishingManager.probMaker(nope) == 0){
                 gottenFish.remove();
-            } else { new FishingManager();
-                List<Double> rsumList = FishingManager.getRsumList();
-                        System.out.println("You gotta" + FishingManager.getFitemList().get(FishingManager.probMaker(rsumList)));
-                        gottenFish.setItemStack(new ItemStack(Material.RAW_BEEF));
+            } else {
+                List<Double> ratioList = FishingManager.makeRatioList(playerBiome, playerWeather);
+                List<Double> rsumList = FishingManager.makeSumList(ratioList);
+                FishableItem harvest = FishingManager.getFishList().get(FishingManager.probMaker(rsumList));
+                        System.out.println("You gotta" + harvest);
+                        gottenFish.setItemStack(FishingManager.getFitemMap().get(harvest));
                     }
                 }
 
     }
-    private List<Double> makeNope(){
-        List<Double> list = new ArrayList<>();
-        list.add(5.0);
-        list.add(5.0);
-        return list;//テスト用　後で消します
-    }
+
 }
