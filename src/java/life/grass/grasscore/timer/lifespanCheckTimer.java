dce86ca967815606440e06d26f02cc97b24e8dec
rikugemba <gurapomu@gmail.com>
2016-12-06 00:19:09 +0900

    [add] 寿命の更新

---
+++ src/java/life/grass/grasscore/timer/lifespanCheckTimer.java
@@ -0,0 +1,21 @@
+package life.grass.grasscore.timer;
+
+import life.grass.grasscore.GrassCore;
+import life.grass.grasscore.player.PlayerManagerGC;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class lifespanCheckTimer implements Runnable {
+    JavaPlugin plugin;
+    public lifespanCheckTimer(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+    @Override
+    public void run() {
+        PlayerManagerGC.getInstance().getPlayerInfoList().stream().forEach(p -> {
+            p.shrinkLifespan();
+            if(p.getLifespan() <= 0){
+                //寿命が尽きたときの処理
+            }
+        });
+    }
+}
