507ce0ae0c71bbdf4842607508da2ffbb15c8b7a
明地えしら <ecilavip@outlook.jp>
2016-12-06 00:24:58 +0900

    [add] enchant position

--- src/java/life/grass/grasscore/item/enchant/EnchantPosition.java
+++ src/java/life/grass/grasscore/item/enchant/EnchantPosition.java
@@ -4,4 +4,17 @@ package life.grass.grasscore.item.enchant;
  * Created by ecila on 2016/12/05.
  */
 public enum EnchantPosition {
+    SPECIAL(0),
+    PREFIX(1),
+    SUFFIX(2);
+
+    private final int positionId;
+
+    EnchantPosition(int i) {
+        positionId = i;
+    }
+
+    public int getId() {
+        return positionId;
+    }
 }
