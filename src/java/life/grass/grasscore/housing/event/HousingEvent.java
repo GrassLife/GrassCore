122fc2a05d8f3080f03a486499d7f7587fb60837
Cyclic_Ester <unconfigured@null.spigotmc.org>
2017-02-12 00:39:59 +0900

    [add] housing event.java

--- src/java/life/grass/grasscore/housing/event/HousingEvent.java
+++ src/java/life/grass/grasscore/housing/event/HousingEvent.java
@@ -15,7 +15,7 @@ public class HousingEvent implements Listener {
         if (event.getBlockPlaced().getType() == Material.COAL_ORE){
             House house = new House(event.getPlayer().getName(), event.getBlock().getLocation());
             System.out.println(house.getCentralLocation() + house.getPlayerName());
-            event.getPlayer().sendMessage("This block is set as center of your house!! Welcome home!!");
+            event.getPlayer().sendMessage("This block is set as center of your house!! Welcome home!! Owner: " + event.getPlayer().getName());
         }
     }
 
